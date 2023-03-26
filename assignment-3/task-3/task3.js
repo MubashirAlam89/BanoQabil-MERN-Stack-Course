c9dee5cb201d81449e6e7ca3624bab90cee6d812
mubashiralam89 <mubashiralam89@gmail.com>
2023-03-26 22:17:46 +0500

    task-3 completed

---
+++ assignment-3/task-3/task3.js
@@ -0,0 +1,7 @@
+let temperature = 72; //temperature in fahrenheit
+
+// formula of fahrenheit to celsius  C = (F - 32) * 5/9
+
+let celsius = (temperature - 32) * (5 / 9);
+
+console.log(`temperatur in celcius ${celsius}`);
